127d12cbeb74011a9b7fc46ae08a988d07110442
taotao7 <moca_tao7@foxmail.com>
2022-04-27 23:15:07 +0800

    chore: request devices description

--- upnp.ts
+++ upnp.ts
@@ -1,8 +1,32 @@
 import * as dgram from "dgram";
+import axios from "axios";
+import * as parser from "xml2json";
+import { uniq } from "lodash";
+
+type controlServerDevices = {
+	root?: {
+		xmlns?: string;
+		specVersion?: { marjor?: number; minor?: number };
+		device?: {
+			deviceType?: string;
+			presentationURL?: string;
+			friendlyName?: string;
+			UDN?: string;
+			serviceList?: [];
+			manufacturer?: string;
+			manufacturerURL?: string;
+			modelDescription?: string;
+			modelName?: string;
+			modelURL?: string;
+		};
+		URLBase?: string;
+	};
+};
 
 class Upnp {
 	client: dgram.Socket;
 	devices: string[] = [];
+	controlServerDevicesList: controlServerDevices[] = [];
 
 	constructor() {
 		this.client = dgram.createSocket("udp4");
@@ -28,7 +52,18 @@ class Upnp {
 		this.discover();
 		setTimeout(() => {
 			this.client.close();
+			this.devices = uniq(this.devices);
+			this.request();
 		}, 5000);
+	}
+
+	// TODO queue request devices description xml
+	private request() {
+		this.devices.forEach(async (device) => {
+			const { data } = await axios.get(device);
+			const json: controlServerDevices = JSON.parse(parser.toJson(data));
+			this.controlServerDevicesList.push(json);
+		});
 	}
 }
 
